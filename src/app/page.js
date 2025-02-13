f145e1603f47981f539c3aea035103ea8ed6606f
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-02-13 11:15:07 +0530

    Update the website

--- src/app/page.js
+++ src/app/page.js
@@ -10,8 +10,8 @@ const page = () => {
     <div>
       <HeroSection/>
       <HotelBookingForm/>
-      {/* <FeaturesSection/>
-      <Testimonial/> */}
+      <FeaturesSection/>
+      <Testimonial/>
       <FAQs/>
     </div>
   )
