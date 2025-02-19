f3cc9cea8f56137675683c716fd61535171340ec
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-02-19 11:13:53 +0530

    Added some files in the admin panel

---
+++ src/app/Admin/dashboard/layout.jsx
@@ -0,0 +1,19 @@
+import 'tailwindcss/tailwind.css';
+import '@/app/globals.css';
+import NavbarAdmin from '@/components/adminPanel/NavbarAdmin';
+import SidebarAdmin from '@/components/adminPanel/SidebarAdmin';
+
+const Layout = ({ children }) => {
+  return (
+    <div className="flex w-full min-h-screen overflow-hidden">
+    <SidebarAdmin className="w-1/4 h-screen bg-gray-800" />
+    <div className="flex-1 overflow-hidden ">
+      <NavbarAdmin className="w-full" />
+      {children}
+    </div>
+  </div>
+  
+  );
+};
+
+export default Layout;
