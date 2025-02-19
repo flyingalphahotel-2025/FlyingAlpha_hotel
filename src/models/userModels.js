6d416af085339c54757195745f39a0faccacef24
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-02-19 11:16:08 +0530

    Added models

---
+++ src/models/userModels.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: [true, 'Full name is required']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        match: [/.+\@.+\..+/, 'Please provide a valid email address'],
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    mobileNumber: {
+        type: String,
+    },
+    password: {
+        type: String,
+        minlength: [6, 'Password must be at least 6 characters long']
+    },
+    profilePic: {
+        type: String,
+        default: ''
+    },
+    address: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Address',
+    }],
+    role: {
+        type: String,
+        enum: ['User', 'SuperAdmin' , 'Vendor'],
+        default: 'User'
+    },
+    status: {
+        type: String,
+        enum: ['Blocked', 'Pending', 'inReview', 'Active'],
+        default: 'Active'
+    },
+}, {
+    timestamps: true
+});
+
+export default mongoose.models.User || mongoose.model('User', userSchema);
