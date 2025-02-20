94f1f97406c1e860244d43937dcffd87d37ddce6
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-02-20 13:51:16 +0530

    Added PrivacyPolicy Models

---
+++ src/models/(policy)/privacyPolicyModels.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const privacyPolicySchema = new mongoose.Schema({
+    content :{
+        type : mongoose.Schema.Types.Mixed,
+        required : true
+
+    }
+},{
+    timestamps: true
+})
+
+export default mongoose.models.PrivacyPolicy || mongoose.model('PrivacyPolicy' , privacyPolicySchema)
