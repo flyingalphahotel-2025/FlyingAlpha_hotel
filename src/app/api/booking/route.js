25153a1d2b705f610f81169c9567f35981be80a7
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-02-24 12:59:18 +0530

    Booking api is for the first testing

--- src/app/api/booking/route.js
+++ src/app/api/booking/route.js
@@ -1,9 +1,20 @@
 import connectDB from "@/Lib/dbConnect"
+import { NextResponse } from "next/server";
 
 export const POST = async(req) => {
+    try {
     console.log("Connecting to the database ........")
     connectDB();
     console.log("Connected to the database .......")
+
+    console.log("Parsing request body.....")
+    const body = req.json();
+    console.log(body);
+    await NextResponse.json({message : "paresed the body " }, {status: 200})
+    } catch (error) {
+        console.error("Error processing request:", error);
+    return NextResponse.json({ message: "Error processing request", error: error.message }, { status: 500 });
+    }
 
     
 }
