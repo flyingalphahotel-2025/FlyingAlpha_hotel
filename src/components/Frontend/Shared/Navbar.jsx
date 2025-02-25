0a57784282a8565f7c4852717179e6c2426c9cd3
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-02-25 16:45:50 +0530

    Modify the button in the mobile view navabr

--- src/components/Frontend/Shared/Navbar.jsx
+++ src/components/Frontend/Shared/Navbar.jsx
@@ -130,17 +130,28 @@ const Navbar = () => {
     </Link>
 
 
-    {/* Mobile CTA Button */}
-    <div className="relative inline-flex items-center justify-center group">
-      <div className="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-cyan-500 to-purple-500 group-hover:shadow-lg group-hover:shadow-cyan-500/50"></div>
+      {/* Mobile CTA Button */}
+    <div className="relative md:hidden flex items-center justify-center group">
+      {/* Animated gradient border */}
+      <motion.div
+        className="absolute transition-all duration-200 rounded-full -inset-px bg-gradient-to-r from-[#FF0080] to-blue-600 group-hover:shadow-lg group-hover:shadow-[#FF0080]/100"
+        initial={{ rotate: 0 }}
+        animate={{ rotate: 360 }}
+        transition={{
+          repeat: Infinity,
+          duration: 3,
+          ease: 'linear',
+        }}
+      />
+      {/* Button */}
       <Link
-        href="/StartFreeTrial"
-        className="relative inline-flex items-center justify-center w-full px-6 py-2 text-base font-normal text-white bg-black border border-transparent rounded-full"
+        href="#"
+        className="relative inline-flex items-center justify-center w-full px-4 py-2 text-sm font-normal text-gray-700 bg-white border border-transparent rounded-full hover:underline"
         role="button"
       >
-            Start free trial
-          </Link>
-        </div>
+        Book Now
+      </Link>
+    </div>
       </motion.nav>
     )}
 
