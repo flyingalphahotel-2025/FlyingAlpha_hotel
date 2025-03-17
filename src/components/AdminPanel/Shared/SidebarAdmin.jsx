eb23dd6d6b5f0ad7858eaca17bd5dbec7c8ef4fb
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-17 18:52:30 +0530

    Updated the sidebar

--- src/components/AdminPanel/Shared/SidebarAdmin.jsx
+++ src/components/AdminPanel/Shared/SidebarAdmin.jsx
@@ -1,14 +1,11 @@
 "use client";
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { FaAppleAlt, FaCarrot, FaPlus, FaShoppingCart, FaListUl, FaCog, FaSearch, FaHome } from "react-icons/fa";
-import { FaTable, FaProductHunt } from "react-icons/fa6";
+import { FaHome, FaShoppingCart, FaCog, FaSearch, FaListAlt, FaBook, FaUser, FaSignOutAlt } from "react-icons/fa";
+import {  MdAdd } from "react-icons/md";
 import { GiOpenBook } from "react-icons/gi";
-import { MdOutlineLogout, MdPendingActions, MdOutlineRateReview } from "react-icons/md";
-import { ImProfile } from "react-icons/im";
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { MdAdd } from "react-icons/md";
 import { toast } from 'react-hot-toast';
 
 const SidebarAdmin = () => {
@@ -37,72 +34,106 @@ const SidebarAdmin = () => {
 
   return (
     <div className="flex">
-    <motion.div
-      className="bg-gradient-to-b from-blue-900 to-teal-700 text-gray-200 h-screen p-5 shadow-lg overflow-y-auto"
-      style={{
-        scrollbarWidth: 'thin',
-        scrollbarColor: 'rgba(255, 255, 255, 0.5) rgba(0, 0, 0, 0.3)',
-      }}
-    >
-      <div className="flex flex-col space-y-4">
-        <h2 className="text-lg font-semibold mb-4">FlyingAlpha Dashboard</h2>
+      <motion.div
+        className="bg-gradient-to-b from-blue-900 to-teal-700 text-gray-200 h-screen p-5 shadow-lg overflow-y-auto"
+        style={{
+          scrollbarWidth: 'thin',
+          scrollbarColor: 'rgba(255, 255, 255, 0.5) rgba(0, 0, 0, 0.3)',
+        }}
+      >
+        <div className="flex flex-col space-y-4">
+          <h2 className="text-lg font-semibold mb-4">FlyingAlpha Dashboard</h2>
 
-        <Link href="/admin/dashboard" passHref>
-          <SidebarItem
-            icon={<FaHome />}
-            label="Home"
-            selected={selectedItem === 'Home'}
-            onClick={() => setSelectedItem('Home')}
-          />
-        </Link>
+          <Link href="/admin/dashboard" passHref>
+            <SidebarItem
+              icon={<FaHome />}
+              label="Home"
+              selected={selectedItem === 'Home'}
+              onClick={() => setSelectedItem('Home')}
+            />
+          </Link>
 
-        <SectionTitle title="Orders" />
-        <Link href="/admin/dashboard/orders" passHref>
-          <SidebarItem
-            icon={<FaShoppingCart />}
-            label="All Orders"
-            selected={selectedItem === 'All Orders'}
-            onClick={() => setSelectedItem('All Orders')}
-          />
-        </Link>
-        <Link href="/admin/dashboard/orders/searchOrders" passHref>
-          <SidebarItem
-            icon={<MdPendingActions />}
-            label="Search Orders"
-            selected={selectedItem === 'Search Orders'}
-            onClick={() => setSelectedItem('Search Orders')}
-          />
-        </Link>
+          <SectionTitle title="Orders" />
+          <Link href="/admin/dashboard/orders/add" passHref>
+            <SidebarItem
+              icon={<MdAdd />}
+              label="Add Booking"
+              selected={selectedItem === 'Add Booking'}
+              onClick={() => setSelectedItem('Add Booking')}
+            />
+          </Link>
+          <Link href="/admin/dashboard/orders/online" passHref>
+            <SidebarItem
+              icon={<FaShoppingCart />}
+              label="Online Booking"
+              selected={selectedItem === 'Online Booking'}
+              onClick={() => setSelectedItem('Online Booking')}
+            />
+          </Link>
+          <Link href="/admin/dashboard/orders" passHref>
+            <SidebarItem
+              icon={<FaListAlt />}
+              label="All Bookings"
+              selected={selectedItem === 'All Bookings'}
+              onClick={() => setSelectedItem('All Bookings')}
+            />
+          </Link>
+          <Link href="/admin/dashboard/orders/search" passHref>
+            <SidebarItem
+              icon={<FaSearch />}
+              label="Search Bookings"
+              selected={selectedItem === 'Search Bookings'}
+              onClick={() => setSelectedItem('Search Bookings')}
+            />
+          </Link>
 
-        <h3 className="text-sm font-medium mt-4 mb-2 text-gray-300">Account</h3>
-        <Link href="/admin/dashboard/profile" passHref>
-          <SidebarItem
-            icon={<ImProfile />}
-            label="Profile"
-            selected={selectedItem === 'Profile'}
-            onClick={() => setSelectedItem('Profile')}
-          />
-        </Link>
-        <Link href="/settings" passHref>
-          <SidebarItem
-            icon={<FaCog />}
-            label="Settings"
-            selected={selectedItem === 'Settings'}
-            onClick={() => setSelectedItem('Settings')}
-          />
-        </Link>
+          <SectionTitle title="Policy" />
+          <Link href="/admin/dashboard/policy/terms" passHref>
+            <SidebarItem
+              icon={<GiOpenBook />}
+              label="Terms and Conditions"
+              selected={selectedItem === 'Terms and Conditions'}
+              onClick={() => setSelectedItem('Terms and Conditions')}
+            />
+          </Link>
+          <Link href="/admin/dashboard/policy/privacy" passHref>
+            <SidebarItem
+              icon={<FaBook />}
+              label="Privacy Policy"
+              selected={selectedItem === 'Privacy Policy'}
+              onClick={() => setSelectedItem('Privacy Policy')}
+            />
+          </Link>
 
-        <button
-          className="mt-6 flex items-center rounded-lg bg-red-600 px-4 py-2 text-sm font-medium text-white transition-all duration-300 hover:bg-red-700 shadow-md"
-          onClick={handleLogout}
-        >
-          <MdOutlineLogout className="h-4 w-4" aria-hidden="true" />
-          <span className="ml-2">Logout</span>
-        </button>
-      </div>
-    </motion.div>
-  </div>
-);
+          <SectionTitle title="Account" />
+          <Link href="/admin/dashboard/profile" passHref>
+            <SidebarItem
+              icon={<FaUser />}
+              label="Profile"
+              selected={selectedItem === 'Profile'}
+              onClick={() => setSelectedItem('Profile')}
+            />
+          </Link>
+          <Link href="/admin/dashboard/settings" passHref>
+            <SidebarItem
+              icon={<FaCog />}
+              label="Settings"
+              selected={selectedItem === 'Settings'}
+              onClick={() => setSelectedItem('Settings')}
+            />
+          </Link>
+
+          <button
+            className="mt-6 flex items-center rounded-lg bg-red-600 px-4 py-2 text-sm font-medium text-white transition-all duration-300 hover:bg-red-700 shadow-md"
+            onClick={handleLogout}
+          >
+            <FaSignOutAlt className="h-4 w-4" aria-hidden="true" />
+            <span className="ml-2">Logout</span>
+          </button>
+        </div>
+      </motion.div>
+    </div>
+  );
 };
 
 const SidebarItem = ({ icon, label, selected, onClick }) => {
@@ -116,7 +147,7 @@ const SidebarItem = ({ icon, label, selected, onClick }) => {
           : 'hover:bg-teal-600 text-gray-200'
       }`}
     >
-      <div className="w-4 h-4">{icon}</div>
+      <div className="text-xl">{icon}</div>
       <span className="font-semibold text-sm">{label}</span>
     </motion.div>
   );
