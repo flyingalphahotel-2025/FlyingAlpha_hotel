8b7958856881d13ca66395680bb46c52125ffb0a
niks-nikhil-anand <niks.anand.developer@gmail.com>
2025-03-23 18:48:16 +0530

    Added steps in the components

---
+++ src/components/AdminPanel/Steps/PaymentInformationStep.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PaymentInformationStep = () => {
+  return (
+    <div>PaymentInformationStep</div>
+  )
+}
+
+export default PaymentInformationStep
---
+++ src/components/AdminPanel/Steps/RoomDetailsStep.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RoomDetailsStep = () => {
+  return (
+    <div>RoomDetailsStep</div>
+  )
+}
+
+export default RoomDetailsStep
